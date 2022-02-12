af7979d4cbac3972e08b2a0b790f997b03c412d4
Dora <kawaiidora@outlook.com>
2022-02-12 10:47:42 +0800

    finished feature 2 and 3

--- screenspecscalculator.py
+++ screenspecscalculator.py
@@ -3,17 +3,31 @@ import numpy as np
 def ppi(h_px, v_px, d_inch):
     return np.sqrt(np.square(h_px) + np.square(v_px)) / d_inch
 
+def ppi_from_pixel_distance(pixel_distance):
+    return 25.4 / pixel_distance
+
 # 人眼最小的可视角度为1角分，60角分为1度
 # 三角函数计算时单位为弧度
 minimum_sight_angle = np.pi / 180 / 60
 
 fixed_value = np.tan(minimum_sight_angle / 2)
 
+
+def pixel_distance_from_ppi(ppi):
+    return 25.4 / ppi
+
+def pixel_distance(view_distance):
+    return 2 * view_distance * 10 * fixed_value
+
+def view_distance(pixel_distance):
+    return pixel_distance / (2 * fixed_value) / 10
+
 if __name__ == '__main__':
+    # print(fixed_value)
     print('屏幕参数计算器')
     print('1 给定分辨率和对角线尺寸计算ppi')
     print('2 给定观看距离计算最佳ppi')
-    print('3 给定分辨率和对角线尺寸计算最短观看距离\n')
+    print('3 给定分辨率和对角线尺寸计算最短观看距离')
     print('4 给出ppi和缩放倍率，计算实际显示大小和设计目标大小的差值\n')
     
     value = int(input('请输入所需的功能对应的序号\n'))
@@ -25,9 +39,17 @@ if __name__ == '__main__':
             print('分辨率为{}x{}的{}寸屏幕，它的ppi约为{:.2f}'
             .format(h_px, v_px, d_inch, ppi(h_px, v_px, d_inch)))
         case 2:
-            pass
+            distance = float(input('请输入观看距离，单位为厘米'))
+            result = ppi_from_pixel_distance(pixel_distance(distance))
+            print('观看距离为{:.1f}厘米时，最佳ppi约为{:.2f}'.format(distance, result))
         case 3:
-            pass
+            h_px = int(input('请输入横向像素数量'))
+            v_px = int(input('请输入纵向像素数量'))
+            d_inch = float(input('请输入屏幕对角线的长度，单位为英寸'))
+            ppi_value = ppi(h_px, v_px, d_inch)
+            result = view_distance(pixel_distance_from_ppi(ppi_value))
+            print('分辨率为{}x{}的{}寸屏幕，最短（近）观看距离约为{:.2f}厘米'
+            .format(h_px, v_px, d_inch, result))
         case 4:
             pass
         case _:
